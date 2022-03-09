7d6ab1a9b0eca8387f595c29200c5586698a5d9e
2612705 <2612705w@student.gla.ac.uk>
2022-03-09 21:10:43 +0000

    PTTList Update

--- PTTList.java
+++ PTTList.java
@@ -1,14 +1,64 @@
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 public class PTTList {
+    private ArrayList<PartTimeTeacher> PList = new ArrayList<>();;
+    private ArrayList<RequirementList> RList = new ArrayList<>();;
+    private ArrayList<PTTList> PTTList;
 
-    public PTTList(){
-        
+    public PTTList(ArrayList<PartTimeTeacher> PList, ArrayList<RequirementList> RList){
+        this.PList = PList;
+        this.RList = RList;
     }
 
-    public void add(){
-
+    public ArrayList<PartTimeTeacher> getPList(){
+        return PList;
     }
 
-    public void move(){
+    public ArrayList<RequirementList> getRList(){
+        return RList;
+    }
 
+    public void add(PartTimeTeacher p, RequirementList r, PTTList l){
+        PList.add(p);
+        RList.add(r);
+        PTTList.add(l);
+    }
+
+    public void remove(PTTList list){
+        int	j;
+        int i = -1;
+		for (j = 0; j < PTTList.size(); j++){
+            if (PTTList.get(j) == list){
+                i = j;               
+                break;
+            }
+        }
+		PTTList.remove(i);	
+    }
+
+    public PTTList find(String name){
+        int	j;
+        int i = -1;
+		for (j = 0; j < PList.size(); j++){
+            if (PList.get(j).getName() == name){
+                i = j;               
+                break;
+            }
+        }
+        if(i != -1){
+            return PTTList.get(j);
+        }else{
+            return null;
+        }      
+    }
+
+    public void print(PrintStream ps){
+        for (int j = 0; j < PTTList.size(); j++){
+            ps.print(String.format("Teacher Name: %s ", PList.get(j).getName()));
+            ps.print(String.format("Teacher Skill: %s ", PList.get(j).getSkill()));
+            ps.print(String.format("Requirement: %s ", RList.get(j).getClass()));
+            ps.println();
+        }
     }
 }

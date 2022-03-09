cf3c45b7ad8f0ec5ef037437b10bbfc4e9a536ca
2612705 <2612705w@student.gla.ac.uk>
2022-03-09 21:20:27 +0000

    add content

--- PTTList.java
+++ PTTList.java
@@ -2,29 +2,32 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class PTTList {
-    private ArrayList<PartTimeTeacher> PList = new ArrayList<>();;
-    private ArrayList<RequirementList> RList = new ArrayList<>();;
-    private ArrayList<PTTList> PTTList;
+    private ArrayList<PartTimeTeacher> PList = new ArrayList<>(); // Create a arraylist to storage the part-time teacher
+    private ArrayList<RequirementList> RList = new ArrayList<>(); // Create a arraylist to storage the requirements of teacher
+    private ArrayList<PTTList> PTTList = new ArrayList<>(); //Create a arraylist to storage both the PList and the RList
 
+    //Constructor
     public PTTList(ArrayList<PartTimeTeacher> PList, ArrayList<RequirementList> RList){
         this.PList = PList;
         this.RList = RList;
     }
 
-    public ArrayList<PartTimeTeacher> getPList(){
+    public ArrayList<PartTimeTeacher> getPList(){ // getter
         return PList;
     }
 
-    public ArrayList<RequirementList> getRList(){
+    public ArrayList<RequirementList> getRList(){ //getter
         return RList;
     }
 
-    public void add(PartTimeTeacher p, RequirementList r, PTTList l){
+    // add value to PList and RList and then add both of them to PTTList
+    public void add(PartTimeTeacher p, RequirementList r, PTTList l){ 
         PList.add(p);
         RList.add(r);
         PTTList.add(l);
     }
 
+    // method to remove the value from PTTList
     public void remove(PTTList list){
         int	j;
         int i = -1;
@@ -37,6 +40,7 @@ public class PTTList {
 		PTTList.remove(i);	
     }
 
+    //method to find the PTTList
     public PTTList find(String name){
         int	j;
         int i = -1;
@@ -53,6 +57,7 @@ public class PTTList {
         }      
     }
 
+    //output form of PTTList
     public void print(PrintStream ps){
         for (int j = 0; j < PTTList.size(); j++){
             ps.print(String.format(PList.get(j).print(), RList.get(j).print()));
